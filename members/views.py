4d085135020875f02a623d1ed4884ee2419c947a
tomus1857 <tomus1857.2001@o2.pl>
2024-07-22 14:39:57 +0200

    Initial commit

---
+++ members/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers' : mymembers
+    }
+    return HttpResponse(template.render(context, request))
+def welcome(request):
+    return HttpResponse("Witaj na stronie głównej aplikacji!")
